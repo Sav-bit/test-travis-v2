1423e3176d6b0e255e7a7f6231a1d1a358923214
Sav-bit <56634996+Sav-bit@users.noreply.github.com>
2019-10-25 20:22:40 +0200

    questa volta DEVI funzionare

---
+++ src/MyHelloWorld.java
@@ -0,0 +1,19 @@
+/**
+ * 
+ */
+
+/**
+ * @author sav
+ *
+ */
+public class MyHelloWorld {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		System.out.println("Hello World!");
+
+	}
+
+}
